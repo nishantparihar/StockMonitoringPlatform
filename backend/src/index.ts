416a3182b8565cb4b7a675da5b8f73446a558a4b
nishantparihar <nishprihar@gmail.com>
2024-05-15 20:13:08 +0530

    added cors, routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,25 @@
-import express from 'express';
-const dotenv = require('dotenv');
+import express, { Request, Response, NextFunction, Router } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors'
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello, TypeScript Node Express!');
 });
+
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong');
+});
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
